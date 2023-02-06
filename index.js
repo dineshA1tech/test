baa589a88096b897601c49c002fc8cd7fc1c0d8d
dineshA1tech <dineshgkp12@gmail.com>
2023-02-06 13:22:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
+var http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
-});
-
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(8080);
